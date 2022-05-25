f0633e9e149b581a62e959d8cee2dfcc00e4267a
elie haddad <haddadelie12@gmail.com>
2022-05-25 09:52:17 +0300

    lab 4

---
+++ lab4/parasite.py
@@ -0,0 +1,7 @@
+from random import randint
+
+class Parasite:
+    def __init__(self, arr, fitness):
+        self.arr = arr
+        self.fitness = fitness
+
---
+++ lab4/struct.py
@@ -0,0 +1,7 @@
+from random import randint
+
+class Struct:
+    def __init__(self, arr, fitness):
+        self.arr = arr
+        self.fitness = fitness
+
